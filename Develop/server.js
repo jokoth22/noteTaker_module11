03359ff0347c4fdf68589713d655d32fcc45ce95
jokoth22 <joyce.okoth22@gmail.com>
2023-11-05 10:18:36 -0500

    update server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,55 @@
+// Import express package
 const express = require ('express');
 const path = require ('path');
-
+const notes= require ('./db.json');
+const fs =require('fs');
+const {v4: uuidv4} = require ('uuid');
+// Initialize our app variable by setting it to the value of express()
 const app= express();
-const port = 3001;
+const PORT = 3001;
 
+//Use middleware to parse JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Serve the front-end files using Express static middleware.
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
 
+app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
+
+//Creates HTML routes that will serve up index.html & notes.html
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.get('/index', (req, res) =>
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
+
+///////Next part/////
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+    });
+  });
+  
+  app.post('/api/notes', (req, res) => {
+    const newNote = { ...req.body, id: uuidv4() };
+  
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      notes.push(newNote);
+  
+      fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+        if (err) throw err;
+        res.json(newNote);
+      });
+    });
+  });
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Note Taker Starter Code
+POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
