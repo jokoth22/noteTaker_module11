e2406689206c4859666ea2bbdd598362953e9d35
jokoth22 <joyce.okoth22@gmail.com>
2023-11-06 18:23:55 -0500

    update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,11 @@
 // Import express package
-const express = require ('express');
-const path = require ('path');
-const notes= require ('./db.json');
-const fs =require('fs');
-const {v4: uuidv4} = require ('uuid');
+const express = require('express');
+const path = require('path');
+//const notes= require ('./db.json');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 // Initialize our app variable by setting it to the value of express()
-const app= express();
+const app = express();
 const PORT = 3001;
 
 //Use middleware to parse JSON and urlencoded form data
@@ -15,9 +15,43 @@ app.use(express.urlencoded({ extended: true }));
 //Serve the front-end files using Express static middleware.
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+///////Next part/////
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
 
-app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
+app.post('/api/notes', (req, res) => {
+  const newNote = { ...req.body, id: uuidv4() };
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json(newNote);
+    });
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.id !== noteId);
+
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json({ msg: 'Note deleted' });
+    });
+  });
+});
 
 //Creates HTML routes that will serve up index.html & notes.html
 app.get('/notes', (req, res) =>
@@ -27,45 +61,6 @@ app.get('/notes', (req, res) =>
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
-
-///////Next part/////
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      res.json(JSON.parse(data));
-    });
-  });
-  
-  app.post('/api/notes', (req, res) => {
-    const newNote = { ...req.body, id: uuidv4() };
-  
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      const notes = JSON.parse(data);
-      notes.push(newNote);
-  
-      fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-        if (err) throw err;
-        res.json(newNote);
-      });
-    });
-  });
-
-  app.delete('/api/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-  
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      let notes = JSON.parse(data);
-      notes = notes.filter((note) => note.id !== noteId);
-  
-      fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-        if (err) throw err;
-        res.json({ msg: 'Note deleted' });
-      });
-    });
-  });
-  
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
